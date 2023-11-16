f8363c4f6f7b39bdb9ee8a722e6aefe31ba9f00d
owleyeview <rashaanlightpool@gmail.com>
2023-11-15 23:20:35 -0800

    added unary operator and decimal checks

--- calculator/script.js
+++ calculator/script.js
@@ -22,57 +22,105 @@ let inputStack = [];
 function processInput(value) {
   // Handle Clear
   if (value === "Clear") {
-    // Clear the display and the stack
-    display.value = "";
+    clearDisplayAndHistory();
+    return;
+  }
+
+  // Handle Unary Operator
+  if (value === "(-)" && inputStack.length > 0) {
+    toggleSign();
+    return;
+  }
+
+  // Handle Multiple Decimal Points
+  if (value === "." && hasMultipleDecimals(inputStack)) {
+    displayError("Multiple decimal points");
+    return;
+  }
+
+  // Handle Equals
+  if (value === "=") {
+    if (inputStack.length > 0) {
+      calculateResult();
+    }
+    return;
+  }
+
+  // Default case: add the value to the stack and display
+  inputStack.push(value);
+  display.value += value;
+}
+
+function clearDisplayAndHistory() {
+  display.value = "";
     inputStack = [];
-    // Add a clear message to the history
     let clearItem = document.createElement("li");
     clearItem.textContent = "-----Cleared-----";
     history.appendChild(clearItem);
-    return;
+}
+
+function toggleSign() {
+  let lastInput = inputStack.pop();
+  if (typeof lastInput === "number") {
+    lastInput = -lastInput;
+  } else {
+    lastInput += " * -1"; 
   }
-  if (value !== "=") {
-    // If it's not "=", store the input in the stack
-    inputStack.push(value);
-    display.value += value;
-  } else if (inputStack.length > 0){
-    // Loop through inputStack and combine digits into numbers
-    let newStack = [];
-    let currentNumber = "";
-    for (let i = 0; i < inputStack.length; i++) {
-      // If the input is part of a number, add it to the currentNumber
-      if (!isNaN(inputStack[i]) || inputStack[i] === ".") {
-        currentNumber += inputStack[i];
-      } else {
-        if (currentNumber !== "") {
-          newStack.push(Number(currentNumber));
-          currentNumber = "";
-        }
-        newStack.push(inputStack[i]);
+  inputStack.push(lastInput);
+  updateDisplay();
+}
+
+function hasMultipleDecimals(stack) {
+  let currentNumber = "";
+  for (let i = stack.length - 1; i >= 0; i--) {
+    if (!isNaN(stack[i]) || stack[i] === ".") {
+      currentNumber = stack[i] + currentNumber;
+    } else {
+      break;
+    }
+  }
+  return currentNumber.includes(".") && value === ".";
+}
+
+function updateDisplay() {
+  display.value = inputStack.join("");
+}
+
+function calculateResult() {
+  let newStack = [];
+  let currentNumber = "";
+  for (let i = 0; i < inputStack.length; i++) {
+    // If the input is part of a number, add it to the currentNumber
+    if (!isNaN(inputStack[i]) || inputStack[i] === ".") {
+      currentNumber += inputStack[i];
+    } else {
+      if (currentNumber !== "") {
+        newStack.push(Number(currentNumber));
+        currentNumber = "";
       }
+      newStack.push(inputStack[i]);
     }
-    if (currentNumber !== "") {
-      newStack.push(Number(currentNumber));
-    }
-
-    let postfix = infixToPostfix(newStack);
-    let result = evaluatePostfixExpression(postfix);
-    // Display the result and store it for more operations
-    display.value = result;
-    
-    inputStack = [];
-    inputStack.push(result);
-
-    // Add the expression and result to the history
-    let expression = "";
-    for (let i = 0; i < newStack.length; i++) {
-      expression += newStack[i];
-    }
-    let listItem = document.createElement("li");
-    listItem.textContent = `${expression} = ${result}`
-    history.appendChild(listItem);
-
   }
+  if (currentNumber !== "") {
+    newStack.push(Number(currentNumber));
+  }
+
+  let postfix = infixToPostfix(newStack);
+  let result = evaluatePostfixExpression(postfix);
+  // Display the result and store it for more operations
+  display.value = result;
+
+  inputStack = [];
+  inputStack.push(result);
+
+  // Add the expression and result to the history
+  let expression = "";
+  for (let i = 0; i < newStack.length; i++) {
+    expression += newStack[i];
+  }
+  let listItem = document.createElement("li");
+  listItem.textContent = `${expression} = ${result}`;
+  history.appendChild(listItem);
 }
 
 function evaluatePostfixExpression(tokens) {
@@ -166,4 +214,12 @@ function evaluate(operator, operand1, operand2) {
     case "^":
       return Math.pow(operand1, operand2);
   }
+}
+
+function displayError(message) {
+  display.value = message;
+  inputStack = [];
+  let errorItem = document.createElement("li");
+  errorItem.textContent = "Error: " + message;
+  history.appendChild(errorItem);
 }
